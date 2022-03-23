6d72ff384de34c592112e8042aab089c1a7af6a4
avan-pra <velovo.vanpraet@gmail.com>
2022-03-23 13:30:39 +0000

    use syscall instead of getrandom, improve argparser

--- srcs/argparse.c
+++ srcs/argparse.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include <stdio.h>
 #include <sys/random.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <sys/types.h>
 #include "args.h"
 #include "utils.h"
 
@@ -17,11 +20,21 @@ enum argID
 
 enum argID switch_str_arg(char *str)
 {
-	if (strcmp(str, "-k") == 0 || strcmp(str, "--key") == 0)			return eKEY;
-	else if (strcmp(str, "-h") == 0 || strcmp(str, "--help") == 0)		return eHELP;
-	else if (strcmp(str, "-v") == 0 || strcmp(str, "--verbose") == 0)	return eVERBOSE;
-	else if (strcmp(str, "-o") == 0 || strcmp(str, "--output") == 0)	return eOUTPUT;
+	if (strncmp(str, "-k", 2) == 0)			return eKEY;
+	else if (strncmp(str, "-h", 2) == 0)	return eHELP;
+	else if (strncmp(str, "-v", 2) == 0)	return eVERBOSE;
+	else if (strncmp(str, "-o", 2) == 0)	return eOUTPUT;
 	return eUNKNOWN;
+}
+
+void retreive_arg(char **argv, char **str, int *i)
+{
+	if (strlen(argv[*i]) > 2)
+		*str = &argv[*i][2];
+	else {
+		*str = argv[*i + 1];
+		(*i)++;
+	}
 }
 
 /* use getopt instead of a shitty handmade argument parser*/
@@ -38,30 +51,30 @@ t_args *getargs(int argc, char **argv)
 	for (int i = 1; i < argc ; ++i) {
 		switch (switch_str_arg(argv[i])) {
 			/* -k <key> */
-			case eKEY: { if (i + 1 == argc) { free(args); print_error("-k: expected key as next argument"); return NULL; } else { args->key = argv[i + 1]; ++i; } break; }
+			case eKEY: { if (i + 1 == argc && strlen(argv[i]) < 2) { free(args); print_error("-k: expected key as next argument"); return NULL; } else { retreive_arg(argv, &args->key, &i); } break; }
 			/* -h --help */
 			case eHELP: { help(); full_help(); free(args); return NULL; break; }
 			/* -v --verbose */
 			case eVERBOSE: { args->verbose = 1; break; }
 			/* -o <key> */
-			case eOUTPUT: { if (i + 1 == argc) { free(args); print_error("-o: expected output file as next argument"); return NULL; } else { args->out_exe = argv[i + 1]; ++i; } break; }
+			case eOUTPUT: { if (i + 1 == argc && strlen(argv[i]) < 2) { free(args); print_error("-o: expected output file as next argument"); return NULL; } else { retreive_arg(argv, &args->out_exe, &i); } break; }
 			/* binary or unknown argument */
 			default: { if (i == argc - 1) { args->exe_name = argv[argc - 1]; } else { free(args); printf("%s%s: unknown argument\n", ERRORTEMPLATE, argv[i]); return NULL; } break; }
 		}
 	}
 
-	if (args->verbose) { printf("Using verbose\n"); }
+	if (args->verbose) { printf("[+] Using verbose\n"); }
 
 	if (args->exe_name == NULL) {
 		print_error("binary not supplied");
 		free(args);
 		return NULL;
 	}
-	if (args->verbose) { printf("Using input file `%s`\n", args->exe_name); }
+	if (args->verbose) { printf("[+] Using input file `%s`\n", args->exe_name); }
 
 	if (args->out_exe == NULL)
 		args->out_exe = DEFAULTOFNAME;
-	if (args->verbose) { printf("Using output file `%s`\n", args->out_exe); }
+	if (args->verbose) { printf("[+] Using output file `%s`\n", args->out_exe); }
 
 	/* error if input == output */
 	if (strcmp(args->exe_name, args->out_exe) == 0) {
@@ -71,18 +84,18 @@ t_args *getargs(int argc, char **argv)
 	}
 
 	if (args->key == NULL) {
+		if (args->verbose) { printf("[*] No key supplied, generating...\n"); }
 		char random_buffer[32];
-		if (getrandom(random_buffer, 32, GRND_NONBLOCK) == -1) {
+		if (syscall(SYS_getrandom, random_buffer, 32, GRND_NONBLOCK) == -1) {
 			print_error(NULL);
 			free(args);
 			return NULL;
 		}
 		for (int i = 0; i < 32; i++) // convert urand0m 32 char to a 64 hex (can probably be done without a for but idk)
-			sprintf(&args->random_key[i * 2], "%02x", (unsigned char)random_buffer[i]);
+			sprintf(&args->random_key[i * 2], "%02X", (unsigned char)random_buffer[i]);
 		args->key = args->random_key;
-		if (args->verbose) { printf("No key supplied, generating...\n"); }
 	}
-	printf("key_value: %s\n", args->key);
+	printf("[+] key_value: %s\n", args->key);
 
 	return args;
 }
