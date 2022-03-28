b4a7d7dcc32b385d90433fdecd787d07d4ce484e
avan-pra <velovo.vanpraet@gmail.com>
2022-03-28 18:37:27 +0000

    improve shellcode creator

--- shellcode-maker.py
+++ shellcode-maker.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 import os
+import tempfile
 
 def main():
 	'''
@@ -9,15 +10,27 @@ def main():
 	if (len(sys.argv) < 2 or len(sys.argv[1]) < 2):
 		print('Usage: shellcode.py <file.s>')
 		return 1
-	os.system("nasm -f elf64 " + sys.argv[1] + "\
+
+	tmpdir = tempfile.mkdtemp()
+	os.system("cp " + sys.argv[1] + " " + tmpdir)
+	c_workplace = os.getcwd()
+	os.chdir(tmpdir)
+
+	os.system("\
+	nasm -f elf64 " + sys.argv[1] + "\
 	&& ld -dynamic-linker /lib64/ld-linux-x86-64.so.2 " + sys.argv[1][:-1] + "o -o" + sys.argv[1][:-1] + "out" + " -lc\
-	&& objcopy --dump-section .text=" + sys.argv[1][:-1] + "tmp " + sys.argv[1][:-1] + "o")
-	a = open(sys.argv[1][:-1] + "tmp",'rb').read().hex();
+	&& objcopy --dump-section .text=" + sys.argv[1][:-1] + "obj " + sys.argv[1][:-1] + "o")
+
+	a = open(sys.argv[1][:-1] + "obj",'rb').read().hex();
 	for i in range(0, len(a)):
 		if i % 2 == 0:
 			print("\\x", end='')
 		print(a[i], end='')
-	os.system("rm " + sys.argv[1][:-1] + "o " + sys.argv[1][:-1] + "tmp " + sys.argv[1][:-1] + "out")
+	print()
+
+	os.chdir(c_workplace)
+	os.system("rm -r " + tmpdir);
+
 	return 0
 
 if __name__ == "__main__":
